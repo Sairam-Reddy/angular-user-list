e748ca6195ee929b221cb15e535c71bd827c0747
Sairam-Reddy <sairamreddyg33@gmail.com>
2023-02-09 15:56:29 +0530

    5 files modified

--- src/app/components/create-user-dialog/create-user-dialog.component.ts
+++ src/app/components/create-user-dialog/create-user-dialog.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-user-dialog',
   templateUrl: './create-user-dialog.component.html',
   styleUrls: ['./create-user-dialog.component.scss'],
 })
-export class CreateUserDialogComponent {}
+export class CreateUserDialogComponent {
+  public userFormGroup: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.userFormGroup = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+    });
+  }
+
+  save(): void {
+    console.log(this.userFormGroup.value);
+  }
+
+  close(): void {}
+}
--- src/app/components/create-user-dialog/create-user-dialog.module.ts
+++ src/app/components/create-user-dialog/create-user-dialog.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -9,6 +10,8 @@ import { CreateUserDialogComponent } from './create-user-dialog.component';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatButtonModule,
     MatDialogModule,
     MatFormFieldModule,
