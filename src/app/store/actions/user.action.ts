70ae28eacf48a12a78b0465526cb0eb0b8ede2c8
Sairam-Reddy <sairamreddyg33@gmail.com>
2023-02-10 13:23:56 +0530

    1 file modified

--- src/app/store/actions/user.action.ts
+++ src/app/store/actions/user.action.ts
@@ -1,12 +1,10 @@
 import { Action } from '@ngrx/store';
+import { createAction } from '@ngrx/store/src/action_creator';
 import { User } from '../../components/users-list/models/user.model';
 
-export const ADD_USER = 'Add User';
-
-export class AddUser implements Action {
-  readonly type = ADD_USER;
-
-  constructor(public payload: User) {}
-}
-
-export type Actions = AddUser;
+export const getUsers = createAction('[User] Get Users');
+export const addUser = createAction('[User] Add User', (user: User) => user);
+export const addUserSuccess = createAction(
+  '[User] Add User Success',
+  (user: User) => user
+);
