4d896d47109d61fd1b0851e35e6ffac5dfc7896c
Sairam-Reddy <sairamreddyg33@gmail.com>
2023-02-10 15:30:33 +0530

    1 file modified

--- src/app/components/users-list/users-list.module.ts
+++ src/app/components/users-list/users-list.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { StoreModule } from '@ngrx/store';
@@ -10,6 +11,7 @@ import { UsersListComponent } from './users-list.component';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     MatButtonModule,
     MatDialogModule,
     CreateUserDialogModule,
