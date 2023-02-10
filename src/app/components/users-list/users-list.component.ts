f90ed35f689426e1d82060f90c6c21f1cb6aba37
Sairam-Reddy <sairamreddyg33@gmail.com>
2023-02-10 14:02:30 +0530

    2 files modified

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
+import { UserState } from '../../store/reducers/user.reducer';
 import { CreateUserDialogComponent } from '../create-user-dialog/create-user-dialog.component';
 import { User } from './models/user.model';
 
@@ -9,7 +11,8 @@ import { User } from './models/user.model';
   styleUrls: ['./users-list.component.scss'],
 })
 export class UsersListComponent {
-  constructor(public dialog: MatDialog) {}
+  movies$ = this.store.select('users');
+  constructor(public dialog: MatDialog, private store: Store<UserState>) {}
 
   public createUser(): void {
     const dialogRef = this.dialog.open(CreateUserDialogComponent);
--- src/app/components/users-list/users-list.module.ts
+++ src/app/components/users-list/users-list.module.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { StoreModule } from '@ngrx/store';
+import { userReducer } from '../../store/reducers/user.reducer';
 import { CreateUserDialogModule } from '../create-user-dialog/create-user-dialog.module';
 import { UsersListComponent } from './users-list.component';
 
@@ -11,6 +13,7 @@ import { UsersListComponent } from './users-list.component';
     MatButtonModule,
     MatDialogModule,
     CreateUserDialogModule,
+    StoreModule.forRoot({ users: userReducer }),
   ],
   declarations: [UsersListComponent],
   exports: [UsersListComponent],
