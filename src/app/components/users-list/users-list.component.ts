bcbcd4b815e59ba7b1ef6e9305880fa546fbacc2
Sairam-Reddy <sairamreddyg33@gmail.com>
2023-02-10 14:21:42 +0530

    3 files modified

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
+import { addUser, getUsers } from '../../store/actions/user.action';
 import { UserState } from '../../store/reducers/user.reducer';
 import { CreateUserDialogComponent } from '../create-user-dialog/create-user-dialog.component';
 import { User } from './models/user.model';
@@ -10,15 +11,20 @@ import { User } from './models/user.model';
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.scss'],
 })
-export class UsersListComponent {
-  movies$ = this.store.select('users');
+export class UsersListComponent implements OnInit {
+  users$ = this.store.select('users');
   constructor(public dialog: MatDialog, private store: Store<UserState>) {}
+
+  public ngOnInit(): void {
+    this.store.dispatch(getUsers());
+  }
 
   public createUser(): void {
     const dialogRef = this.dialog.open(CreateUserDialogComponent);
 
     dialogRef.afterClosed().subscribe((user: User) => {
       if (user) {
+        this.store.dispatch(addUser(user));
       }
     });
   }
--- src/app/store/reducers/user.reducer.ts
+++ src/app/store/reducers/user.reducer.ts
@@ -1,6 +1,6 @@
 import { on, createReducer } from '@ngrx/store';
 import { User } from '../../components/users-list/models/user.model';
-import { getUsers } from '../actions/user.action';
+import { addUser, getUsers } from '../actions/user.action';
 
 export interface UserState {
   users: ReadonlyArray<User>;
@@ -10,5 +10,6 @@ const initialState: ReadonlyArray<User> = [];
 
 export const userReducer = createReducer(
   initialState,
-  on(getUsers, (state) => [...state])
+  on(getUsers, (state) => [...state]),
+  on(addUser, (state, movie) => [...state, movie])
 );
