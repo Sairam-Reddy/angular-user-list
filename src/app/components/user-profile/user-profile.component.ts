f2d5f50b5377d4998a6143f430cbaefd9bb57263
Sairam-Reddy <sairamreddyg33@gmail.com>
2023-02-15 15:25:41 +0530

    7 files modified, 1 added

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  ContentChildren,
+  Input,
+  QueryList,
+} from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 import { User } from '../users-list/models/user.model';
 
 @Component({
@@ -6,6 +13,21 @@ import { User } from '../users-list/models/user.model';
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss'],
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements AfterContentInit {
   @Input() user: User;
+
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent>;
+
+  ngAfterContentInit() {
+    let activeTabs = this.tabs.filter((tab) => tab.active);
+
+    if (activeTabs.length === 0) {
+      this.selectTab(this.tabs.first);
+    }
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs.toArray().forEach((tab) => (tab.active = false));
+    tab.active = true;
+  }
 }
--- src/app/components/user-profile/user-profile.module.ts
+++ src/app/components/user-profile/user-profile.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TabComponent } from '../tab/tab.component';
 import { UserProfileComponent } from './user-profile.component';
 
 @NgModule({
   imports: [CommonModule, MatTabsModule],
-  declarations: [UserProfileComponent],
-  exports: [UserProfileComponent],
+  declarations: [UserProfileComponent, TabComponent],
+  exports: [UserProfileComponent, TabComponent],
 })
 export class UserProfileModule {}
